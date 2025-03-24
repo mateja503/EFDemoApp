e53e59d0db61938f8aec2ec26388fa5864542cbb
mateja503 <matejanikolikj503@gmail.com>
2025-03-24 13:25:06 +0100

    Add project files.

---
+++ EFDataAccessLibrary/Models/Email.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDataAccessLibrary.Models
+{
+    public class Email
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        [Column(TypeName = "varchar(10)")]
+        public string EmailAddress { get; set; }
+    }
+}
